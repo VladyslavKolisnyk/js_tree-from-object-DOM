4cb3457e0144bbf78d0325cd2236c1cb98f5625a
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-11-27 17:34:41 +0200

    make build

---
+++ index.712cc284.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=document.createElement("ul");for(let[l,a]of(t.append(r),Object.entries(n))){let t=document.createElement("li");t.innerText=l,r.append(t),Object.keys(a).length>0&&e(t,a)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.712cc284.js.map
